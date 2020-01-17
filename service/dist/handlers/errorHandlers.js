7904426a3f4355b4f64c4a4894bda88baedc08a2
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2020-01-16 18:46:16 -0600

    removed dist folder from git

--- service/dist/handlers/errorHandlers.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.asyncHandler = (handler) => (req, res, next) => handler(req, res, next).catch(next);
-//# sourceMappingURL=errorHandlers.js.map
