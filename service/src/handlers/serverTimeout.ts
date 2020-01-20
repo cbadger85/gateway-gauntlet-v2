a1db16e939739fd8f138f4518da2cfa2f08d3213
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2020-01-20 16:05:26 -0600

    adjust types on serverTimeout

--- service/src/handlers/serverTimeout.ts
+++ service/src/handlers/serverTimeout.ts
@@ -1,9 +1,9 @@
 import { Params, RequestHandler } from 'express-serve-static-core';
 import HttpError from '../errors/HttpError';
 
-export const serverTimout = <P extends Params, ResBody, ReqBody>(
+export const serverTimout = <P extends Params, ReqBody>(
   time = 120000,
-): RequestHandler<P, ResBody, ReqBody> => (req, res, next): void => {
+): RequestHandler<P, never, ReqBody> => (req, res, next): void => {
   res.setTimeout(time, function() {
     console.log('⏰'.padEnd(4), 'Request has timed out.');
     next(new HttpError('Server has timed out'));
