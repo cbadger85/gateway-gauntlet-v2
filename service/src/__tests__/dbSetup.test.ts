716e11918c0ca9132fa19bea38acb57f0b471a6c
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2020-03-21 20:56:45 -0500

    move migrations to dbsetup

--- service/src/__tests__/dbSetup.test.ts
+++ service/src/__tests__/dbSetup.test.ts
@@ -1,5 +1,6 @@
 import { Container } from 'typedi';
-import { createConnection, useContainer, getConnectionOptions } from 'typeorm';
+import { createConnection, useContainer, MigrationExecutor } from 'typeorm';
+import * as typeorm from 'typeorm';
 import dbSetup from '../dbSetup';
 
 jest.mock('typedi', () => ({
@@ -10,6 +11,9 @@ jest.mock('typeorm', () => ({
   createConnection: jest.fn(),
   useContainer: jest.fn(),
   getConnectionOptions: jest.fn().mockResolvedValue({ option: true }),
+  MigrationExecutor: jest.fn().mockImplementation(() => ({
+    executePendingMigrations: jest.fn().mockResolvedValue(null),
+  })),
 }));
 
 describe('dbSetup', () => {
@@ -21,6 +25,13 @@ describe('dbSetup', () => {
   });
 
   it('should return a connection', async () => {
+    (createConnection as jest.Mock).mockResolvedValue('connection');
+    const connection = await dbSetup();
+
+    expect(connection).toBe('connection');
+  });
+
+  it('should return an error if createConnection fails', async () => {
     (createConnection as jest.Mock).mockRejectedValue(new Error());
     const error = await dbSetup().catch(e => e);
 
--- service/src/dbSetup.ts
+++ service/src/dbSetup.ts
@@ -1,5 +1,10 @@
 import { Container } from 'typedi';
-import { Connection, createConnection, useContainer } from 'typeorm';
+import {
+  Connection,
+  createConnection,
+  useContainer,
+  MigrationExecutor,
+} from 'typeorm';
 import { getEmojiLog } from './utils/getEmojiLog';
 
 export const dbSetup = async (): Promise<Connection> => {
@@ -15,6 +20,15 @@ export const dbSetup = async (): Promise<Connection> => {
   }
 
   console.log(getEmojiLog('🎉', 'Database connected!'));
+
+  console.log(getEmojiLog('🕺', 'Running migrations...'));
+  const migrationExecutor = new MigrationExecutor(connection);
+
+  await migrationExecutor.executePendingMigrations().catch(() => {
+    console.log(getEmojiLog('💥', 'Migrations failed to run'));
+  });
+
+  console.log(getEmojiLog('🎉', 'Migrations successful'));
 
   return connection;
 };
