442da0f715ff8622f6bc7744fd16d1a99149cef0
Badger <tcnb1@c02yd5dqjg5j.unigroupinc.com>
2020-03-21 21:27:32 -0500

    update migrations

--- service/src/__tests__/dbSetup.test.ts
+++ service/src/__tests__/dbSetup.test.ts
@@ -20,17 +20,38 @@ jest.mock('typeorm', () => ({
 
 describe('dbSetup', () => {
   it('should call useContainer with Container', async () => {
-    (createConnection as jest.Mock).mockResolvedValue('connection');
+    (createConnection as jest.Mock).mockResolvedValue({
+      runMigrations: jest.fn(),
+      migrations: [],
+    });
     await dbSetup();
 
     expect(useContainer).toBeCalledWith(Container);
   });
 
   it('should return a connection', async () => {
-    (createConnection as jest.Mock).mockResolvedValue('connection');
+    (createConnection as jest.Mock).mockResolvedValue({
+      runMigrations: jest.fn(),
+      migrations: [],
+    });
     const connection = await dbSetup();
 
-    expect(connection).toBe('connection');
+    expect(connection).toEqual({
+      runMigrations: expect.any(Function),
+      migrations: expect.any(Array),
+    });
+  });
+
+  it('should call runMigrations', async () => {
+    const runMigrations = jest.fn();
+    (createConnection as jest.Mock).mockResolvedValue({
+      runMigrations,
+      migrations: [],
+    });
+
+    await dbSetup();
+
+    expect(runMigrations).toBeCalled();
   });
 
   it('should return an error if createConnection fails', async () => {
